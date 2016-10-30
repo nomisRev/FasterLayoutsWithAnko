5c4610d4223c0277ed399ffadb352769854f4467
Simon Vergauwen <nomisRev@users.noreply.github.com>
2016-10-30 18:04:00 +0100

    Update DetailActivityLayout.kt

--- app/src/main/java/be/vergauwen/simon/fasterankolayouts/ui/detail/DetailActivityLayout.kt
+++ app/src/main/java/be/vergauwen/simon/fasterankolayouts/ui/detail/DetailActivityLayout.kt
@@ -18,22 +18,22 @@ import org.jetbrains.anko.support.v4.nestedScrollView
 
 class DetailActivityLayout : ViewBinder<DetailActivity> {
 
-    override fun DetailActivity.bind(): View =
-            UI {
+    override fun bind(detailActivity: DetailActivity): View =
+            detailActivity.UI {
                 coordinatorLayout {
                     appBarLayout {
                         collapsingToolbarLayout {
                             setContentScrimColor(color(R.color.colorPrimary))
                             id = R.id.toolbar_layout
 
-                            toolbar = toolbar {
+                            detailActivity.toolbar = toolbar {
                                 setSupportActionBar(this)
                                 supportActionBar?.setDisplayHomeAsUpEnabled(true)
                             }.lparams(width = matchParent, height = actionBarSize())
                         }.lparams(width = matchParent, height = matchParent)
                     }.lparams(width = matchParent, height = dip(200))
 
-                    detailContainer = nestedScrollView {
+                    detailActivity.detailContainer = nestedScrollView {
                         id = R.id.item_detail_container
                     }.lparams(width = matchParent, height = matchParent) {
                         behavior = AppBarLayout.ScrollingViewBehavior()
@@ -41,7 +41,8 @@ class DetailActivityLayout : ViewBinder<DetailActivity> {
                 }
             }.view
 
-    override fun DetailActivity.unbind() {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun unbind(detailActivity: DetailActivity) {
+        detailActivity.toolbar = null
+        detailActivity.detailContainer = null
     }
 }
